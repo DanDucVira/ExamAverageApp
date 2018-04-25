226e65dd5caf1a4c01dfbb79e7aa627f238134b5
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2018-04-25 11:04:18 -0400

    Add files via upload

---
+++ ExamResultAverage/ExamResultAverage/ExamResults.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace ExamResultAverage
+{
+    partial class ExamResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudentName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtExam1 = new System.Windows.Forms.TextBox();
+            this.txtExam2 = new System.Windows.Forms.TextBox();
+            this.listStudentName = new System.Windows.Forms.ListBox();
+            this.listExam1 = new System.Windows.Forms.ListBox();
+            this.listExam2 = new System.Windows.Forms.ListBox();
+            this.listAverage = new System.Windows.Forms.ListBox();
+            this.listResult = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtStudentName
+            // 
+            this.txtStudentName.Location = new System.Drawing.Point(173, 78);
+            this.txtStudentName.Multiline = true;
+            this.txtStudentName.Name = "txtStudentName";
+            this.txtStudentName.Size = new System.Drawing.Size(146, 34);
+            this.txtStudentName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exam 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Exam 1";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(574, 240);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(137, 35);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtExam1
+            // 
+            this.txtExam1.Location = new System.Drawing.Point(173, 155);
+            this.txtExam1.Multiline = true;
+            this.txtExam1.Name = "txtExam1";
+            this.txtExam1.Size = new System.Drawing.Size(146, 34);
+            this.txtExam1.TabIndex = 7;
+            // 
+            // txtExam2
+            // 
+            this.txtExam2.Location = new System.Drawing.Point(173, 230);
+            this.txtExam2.Multiline = true;
+            this.txtExam2.Name = "txtExam2";
+            this.txtExam2.Size = new System.Drawing.Size(146, 34);
+            this.txtExam2.TabIndex = 8;
+            // 
+            // listStudentName
+            // 
+            this.listStudentName.FormattingEnabled = true;
+            this.listStudentName.Location = new System.Drawing.Point(16, 411);
+            this.listStudentName.Name = "listStudentName";
+            this.listStudentName.Size = new System.Drawing.Size(138, 108);
+            this.listStudentName.TabIndex = 9;
+            // 
+            // listExam1
+            // 
+            this.listExam1.FormattingEnabled = true;
+            this.listExam1.Location = new System.Drawing.Point(173, 411);
+            this.listExam1.Name = "listExam1";
+            this.listExam1.Size = new System.Drawing.Size(120, 108);
+            this.listExam1.TabIndex = 10;
+            // 
+            // listExam2
+            // 
+            this.listExam2.FormattingEnabled = true;
+            this.listExam2.Location = new System.Drawing.Point(328, 411);
+            this.listExam2.Name = "listExam2";
+            this.listExam2.Size = new System.Drawing.Size(120, 108);
+            this.listExam2.TabIndex = 11;
+            // 
+            // listAverage
+            // 
+            this.listAverage.FormattingEnabled = true;
+            this.listAverage.Location = new System.Drawing.Point(484, 411);
+            this.listAverage.Name = "listAverage";
+            this.listAverage.Size = new System.Drawing.Size(120, 108);
+            this.listAverage.TabIndex = 12;
+            // 
+            // listResult
+            // 
+            this.listResult.FormattingEnabled = true;
+            this.listResult.Location = new System.Drawing.Point(622, 411);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(120, 108);
+            this.listResult.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 375);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Student Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(194, 375);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Exam 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(338, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 24);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Exam 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(496, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 24);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Average";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(643, 375);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 24);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Result";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(574, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 35);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExamResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(768, 546);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.listAverage);
+            this.Controls.Add(this.listExam2);
+            this.Controls.Add(this.listExam1);
+            this.Controls.Add(this.listStudentName);
+            this.Controls.Add(this.txtExam2);
+            this.Controls.Add(this.txtExam1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStudentName);
+            this.Controls.Add(this.label1);
+            this.Name = "ExamResults";
+            this.Text = "Exam Results";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudentName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtExam1;
+        private System.Windows.Forms.TextBox txtExam2;
+        private System.Windows.Forms.ListBox listStudentName;
+        private System.Windows.Forms.ListBox listExam1;
+        private System.Windows.Forms.ListBox listExam2;
+        private System.Windows.Forms.ListBox listAverage;
+        private System.Windows.Forms.ListBox listResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ExamResultAverage/ExamResultAverage/ExamResults.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamResultAverage
+{
+    public partial class ExamResults : Form
+    {
+        public ExamResults()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int exam1, exam2, average;
+
+
+            exam1 = Convert.ToInt32(txtExam1.Text);
+            exam2 = Convert.ToInt32(txtExam2.Text);
+            average = (exam1 + exam2) / 2;
+
+            listStudentName.Items.Add(txtStudentName.Text);
+            listExam1.Items.Add(txtExam1.Text);
+            listExam2.Items.Add(txtExam2.Text);
+
+            listAverage.Items.Add(average);
+
+            if (average >= 60)
+            {
+                listResult.Items.Add("Passed");
+            }
+            else {
+                listResult.Items.Add("Failed");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtStudentName.Clear();
+            txtExam1.Clear();
+            txtExam2.Clear();
+            listStudentName.Items.Clear();
+            listExam1.Items.Clear();
+            listExam2.Items.Clear();
+            listAverage.Items.Clear();
+            listResult.Items.Clear();
+        }
+    }
+}
